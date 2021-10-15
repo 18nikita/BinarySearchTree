670cc0867202d154ba7ab5eadc86b1b3c57e4d25
unknown <rajputnikita5666@gmail.com>
2021-10-16 03:12:56 +0530

    Added:creating BST

---
+++ src/main/java/com/bridgelabz/binarytree/BinarySearchTree.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.binarytree;
+
+public class BinarySearchTree <K extends Comparable<K>>{
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> binaryTree = new BinarySearchTree<Integer>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+
+        System.out.println(binaryTree);
+    }
+    private BinaryNode<K> root;
+
+    public void add(K key)
+    {
+        this.root = this.insertData(root,key);
+    }
+
+    public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+    {
+        if(current ==  null)
+            return new BinaryNode<K>(key);
+        int compareResult = key.compareTo(current.key);
+
+        if(compareResult==0)
+            return current;
+
+        if(compareResult<0)
+        {
+            current.left = insertData(current.left, key);
+        }
+        else
+        {
+            current.right = insertData(current.right, key);
+        }
+        return current;
+    }
+    @Override
+    public String toString()
+    {
+        return "MyBinary [root=" + root + "]";
+    }
+}
